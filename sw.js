8291bcc85fe428405786fe1632db7cbda762f91f
klaisentraus <fitrental@gmail.com>
2026-09-14 13:54:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    Si no hay internet, se usa la copia guardada.
    Los íconos y el manifest: primero lo guardado, que casi nunca cambian. */
 
-var CACHE = 'miplata-v27';
+var CACHE = 'miplata-v28';
 var ASSETS = [
   './',
   './index.html',
